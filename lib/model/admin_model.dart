00c303fe9e23dd8673eb22ebfeca9d5b208f2cce
basmadessouky <basma1900957@miuegypt.edu.eg>
2022-10-30 02:52:19 +0200

    Organizing files

---
+++ lib/model/admin_model.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:project/components.dart';
+
+
+class UsersModel
+{
+  late final String image;
+  late final String name;
+  late final String email;
+  late bool isMale;
+  late bool isdeleted;
+  late int id;
+
+
+  UsersModel({
+  required this.image,
+  required this.name,
+  required this.email,
+  required this.isMale,
+  required this.isdeleted,
+  required this.id,
+  });
+}
+List<UsersModel> users =
+  [
+        UsersModel(
+        image: 'assets/images/NoProfilePicBoy.jpg',
+        name: 'User 1',
+        email:'User\'s 1 Email',
+        isMale: true,
+        isdeleted: true,
+        id: 0,
+        ),
+        UsersModel(
+        image: 'assets/images/NoProfilePicBoy.jpg',
+        name: 'User 2',
+        email:'User\'s 2 Email',
+        isMale: true,
+        isdeleted: true,
+        id: 1,
+        ),
+        UsersModel(
+        image: 'assets/images/NoProfilePicGirl.png',
+        name: 'User 3',
+        email:'User\'s 3 Email',
+        isMale: false,
+        isdeleted: true,
+        id: 2,
+        ),
+        UsersModel(
+        image: 'assets/images/NoProfilePicGirl.png',
+        name: 'User 4',
+        email:'User\'s 4 Email',
+        isMale: false,
+        isdeleted: true,
+        id: 3,
+        ),
+        UsersModel(
+        image: 'assets/images/NoProfilePicBoy.jpg',
+        name: 'User 5',
+        email:'User\'s 5 Email',
+        isMale: true,
+        isdeleted: true,
+        id: 4,
+        ),
+        UsersModel(
+        image: 'assets/images/NoProfilePicGirl.png',
+        name: 'User 6',
+        email:'User\'s 6 Email',
+        isMale: false,
+        isdeleted: true,
+        id: 5,
+        ),
+  ];
---
+++ lib/model/details_data.dart
@@ -0,0 +1,29 @@
+// import 'package:flutter/material.dart';
+
+class DetailsData {
+  String title = 'Moez Street';
+
+  double rating = 4.5;
+  String genre = 'Historical landmark';
+  int timeByCar = 25;
+  int timeWalking = 50;
+  String open = 'Closes soon';
+  int distance = 17;
+
+  final imageUrl = [
+    'assets/Salah-El-Din-Citadel.jpg',
+    'assets/El-Hussien.jpg',
+    'assets/cairo tower.jpg',
+  ];
+
+  final names = [
+    'Salah-El-Din-Citadel',
+    'El-Hussien',
+    'cairo tower',
+  ];
+  // final namLocation = [
+  //   'France',
+  //   'France',
+  //   'France',
+  // ];
+}
