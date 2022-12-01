29bc4de502e7c26a96440e5873827e84ec75c43c
basmadessouky <basma1900957@miuegypt.edu.eg>
2022-12-01 18:35:48 +0200

    Last version of Favorites before Phase 1

--- lib/favorites_screen.dart
+++ lib/favorites_screen.dart
@@ -1,25 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:project/components.dart';
+import 'package:project/favorites_model.dart';
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 
-import 'favorites_model.dart';
-
-class FavoritesScreen extends StatefulWidget
-
-{
+class FavoritesScreen extends StatefulWidget {
   @override
   _FavoritesScreenState createState() => _FavoritesScreenState();
-
 }
 
-class _FavoritesScreenState extends State<FavoritesScreen> 
-{
+class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: maincolor,
       ),
-      body: ListView.separated(
+      body: ConditionalBuilder(
+        condition: favorites.isNotEmpty,
+        builder: (context) => ListView.separated(
           itemBuilder: (context, index) => buildFavoriteItem(favorites[index]),
           separatorBuilder: (context, index) => Padding(
             padding: const EdgeInsetsDirectional.only(
@@ -33,85 +31,108 @@ class _FavoritesScreenState extends State<FavoritesScreen>
             ),
           ),
           itemCount: favorites.length,
+        ),
+        fallback: (context) => Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.not_interested,
+                color: Colors.red,
+                size: 120.0,
+              ),
+              Text(
+                'No Favorites',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-  );
+    );
   }
 
-Widget buildFavoriteItem(FavoritesModel model) => Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children:
-    [
-      SizedBox(
-                height: 20.0,
-              ),
-      Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-          Container(
-            height: 100.0,
-            width: 120.0,
-            child: Stack(
-            alignment: AlignmentDirectional.topStart,
-            children: 
-            [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                  '${model.image}',
-                  ),
-              ),
-              ],
-            ),
-          ),
+  Widget buildFavoriteItem(FavoritesModel model) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
           SizedBox(
-            width: 20.0,
+            height: 20.0,
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '${model.place}',
-                    style: TextStyle(
-                      fontSize: 22.0,
-                      fontWeight: FontWeight.bold,
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 100.0,
+                width: 120.0,
+                child: Stack(
+                  alignment: AlignmentDirectional.topStart,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Image.asset(
+                        '${model.image}',
                       ),
                     ),
-                    Row(
-                    children: [
-                      Text(
-                        '${model.body}',
-                        style: TextStyle(
-                          fontSize: 16.0,
-                        ),
-                      ),
-                  Spacer(),
-                  IconButton(
-                  onPressed: (){
-                    setState(() {
-                      model.isfavorites = !model.isfavorites;
-                    });
-                  },
-                   icon: CircleAvatar(
-                    radius: 15.0,
-                    backgroundColor: model.isfavorites? model.favorites : maincolor,
-                    child: Icon(
-                      Icons.favorite_border,
-                      size: 20.0,
-                      color: Colors.white,
-                    ),
-                   ),
-                   ),
                   ],
                 ),
-                ],
               ),
-            ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${model.place}',
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            '${model.body}',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                model.isfavorites = !model.isfavorites;
+                                favorites.removeWhere(
+                                    (model) => model.isfavorites == false);
+                              });
+                            },
+                            icon: CircleAvatar(
+                              radius: 15.0,
+                              backgroundColor: model.isfavorites
+                                  ? model.favorites
+                                  : maincolor,
+                              child: Icon(
+                                Icons.favorite_border,
+                                size: 20.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
-    ),
-    ],
-  );
+      );
 }
