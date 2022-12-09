13f7939ccfb0ce390ebbfa1ec2629eec19358d93
hashim <hashim_becool@hotmail.com>
2022-12-09 15:09:25 +0200

    Signup text to form (widget , text)

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'home_page.dart';
 class signup extends StatelessWidget{
  @override
@@ -15,98 +14,100 @@ class signup extends StatelessWidget{
         backgroundColor: Color(0xfff4f3f6),
         body: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-            SizedBox(height: 50,),
-                        Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  // controller: nameController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'User Name',
+            child: Form(
+              child: Column(
+                children: [
+              SizedBox(height: 50,),
+                          Container(
+                  padding: const EdgeInsets.all(10),
+                  child: TextFormField(
+                    // controller: nameController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
                   ),
                 ),
-              ),
-          
-                        Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                  obscureText: true,
-                  // controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
+                      
+                          Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    obscureText: true,
+                    // controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
                   ),
                 ),
-              ),
-          SizedBox(height: 20,),
-          Container(
-                padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextField(
-                  obscureText: true,
-                  // controller: passwordController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Confirm Password',
+                      SizedBox(height: 20,),
+                      Container(
+                  padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextFormField(
+                    obscureText: true,
+                    // controller: passwordController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Confirm Password',
+                    ),
                   ),
                 ),
+                SizedBox(height: 20,),
+                      Container(
+                      
+                      ),
+                      SizedBox(height:20 ),
+                          Container(
+                            width: 300,
+                  height: 50,
+                  // padding: const EdgeInsets.fromLTRB( 0, 0, 0, 0),
+                  child: ElevatedButton(
+                    child: const Text('Sign up'),
+                    style: ElevatedButton.styleFrom(primary:Color(0xffc29592) ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()),
+                      );
+                      // print(nameController.text);
+                      // print(passwordController.text);
+                    },
+                  )
+                  ),
+                SizedBox(height:20 ),
+                       
+                  Container(
+                     width: 300,
+                  height: 50,
+                    child: ElevatedButton.icon(
+              onPressed: (){
+            
+              }, 
+              icon: Icon(Icons.facebook), 
+              label: Text("Facebook"),
+             
+            
+            ),
+                  )
+            
+            ,
+            SizedBox(height:20 ),
+                    Container(
+                            width: 300,
+                  height: 50,
+                  // padding: const EdgeInsets.fromLTRB( 0, 0, 0, 0),
+                  child: ElevatedButton(
+                    child: const Text('Google'),
+                    style: ElevatedButton.styleFrom(primary:Color(0x000000) ),
+                    onPressed: () {
+                      // print(nameController.text);
+                      // print(passwordController.text);
+                    },
+                  )
+                  ),
+            
+                ],
               ),
-              SizedBox(height: 20,),
-          Container(
-          
-          ),
-          SizedBox(height:20 ),
-                        Container(
-                          width: 300,
-                height: 50,
-                // padding: const EdgeInsets.fromLTRB( 0, 0, 0, 0),
-                child: ElevatedButton(
-                  child: const Text('Sign up'),
-                  style: ElevatedButton.styleFrom(primary:Color(0xffc29592) ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomeScreen()),
-                    );
-                    // print(nameController.text);
-                    // print(passwordController.text);
-                  },
-                )
-                ),
-              SizedBox(height:20 ),
-           
-                Container(
-                   width: 300,
-                height: 50,
-                  child: ElevatedButton.icon(
-  onPressed: (){
-
-  }, 
-  icon: Icon(Icons.facebook), 
-  label: Text("Facebook"),
- 
-
-),
-                )
-
-,
-SizedBox(height:20 ),
-        Container(
-                          width: 300,
-                height: 50,
-                // padding: const EdgeInsets.fromLTRB( 0, 0, 0, 0),
-                child: ElevatedButton(
-                  child: const Text('Google'),
-                  style: ElevatedButton.styleFrom(primary:Color(0x000000) ),
-                  onPressed: () {
-                    // print(nameController.text);
-                    // print(passwordController.text);
-                  },
-                )
-                ),
-
-              ],
             ),
           ),
         ),
