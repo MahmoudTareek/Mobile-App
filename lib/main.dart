bf14505f4a94a877ac9e7c9c48f87b00090c132c
reemansour <mansourreem02gmail.com>
2022-12-17 00:52:26 +0200

    updated main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:project/login.dart';
 import 'package:project/on_boarding_screen.dart';
 import 'favorites_screen.dart';
 import 'details_screen.dart';
-import 'map.dart';
 import 'view_profile.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      // home: FavoritesScreen(),
-      initialRoute: 'OnBoardingScreen',
-      routes: {
-        'OnBoardingScreen': (context) => OnBoardingScreen(),
-      },
-      debugShowCheckedModeBanner: false,
+    ProviderScope(
+      child: MaterialApp(
+        // home: FavoritesScreen(),
+        initialRoute: 'OnBoardingScreen',
+        routes: {
+          'OnBoardingScreen': (context) => OnBoardingScreen(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
     ),
   );
 }
