539fe4dc188200f66567d848e350e7d05e6f04a9
basmadessouky <basma1900957@miuegypt.edu.eg>
2023-01-20 06:56:01 +0200

    Edited main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:project/views/home_page.dart';
+import 'package:project/views/view_profile.dart';
+import 'services/firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'services/firebase_options.dart';
 import 'views/on_boarding_screen.dart';
+import 'views/details_screen.dart';
 import 'dart:developer';
 
 void main() async {
@@ -15,7 +18,7 @@ void main() async {
         // home: FavoritesScreen(),
         initialRoute: 'OnBoardingScreen',
         routes: {
-          'OnBoardingScreen': (context) => OnBoardingScreen(),
+          'OnBoardingScreen': (context) => const HomeScreen(),
         },
         debugShowCheckedModeBanner: false,
       ),
